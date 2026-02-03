4924a7e7b6ef376025c10f213263e329e76e7a81
rigamonk <607316+hroark@users.noreply.github.com>
2026-02-03 01:55:37 -0600

    Some more cleanup/movement Fixed transparancy on logo3

--- PracticeBuddy/PracticeBuddy/Data/ApplicationUser.cs
+++ PracticeBuddy/PracticeBuddy/Data/ApplicationUser.cs
@@ -7,8 +7,15 @@ namespace PracticeBuddy.Data
     /// Provides an application user class that extends the IdentityUser class.
     /// </summary>
     /// <seealso cref="Microsoft.AspNetCore.Identity.IdentityUser" />
-    public class ApplicationUser(string userName, string password, string passwordVerification) : IdentityUser(userName)
+    public class ApplicationUser(string userName, string password) : IdentityUser(userName)
     {
+        [Required]
+        [EmailAddress]
+        public override string? Email { get; set; } = string.Empty; 
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; } = false;
+
         [Required]
         [StringLength(30, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
         public required string Password { get; set; } = password;
@@ -16,6 +23,10 @@ namespace PracticeBuddy.Data
         [Required]
         [StringLength(30, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
         [Compare(nameof(Password), ErrorMessage = "The passwords do not match.")]
-        public required string PasswordVerification { get; set; } = passwordVerification;
+        [DataType(DataType.DateTime)]
+        public DateTime? LastLogin { get; set; } = DateTime.Now;
+
+        [DataType(DataType.DateTime)]
+        public DateTime? CreatedAt { get; set; }
     }
 }
