9ddc2ec286d3070619709f4832401cab04d7eadc
rigamonk <607316+hroark@users.noreply.github.com>
2026-03-18 09:50:42 -0500

    Web client portion removed. Routes fixed

--- PracticeBuddy/PracticeBuddy/Models/Session.cs
+++ PracticeBuddy/PracticeBuddy/Models/Session.cs
@@ -1,30 +1,27 @@
-using PracticeBuddy.Models;
+namespace PracticeBuddy.Models;
 
-namespace Practice_Buddy.Data
+public class Session
 {
-    public class Session
+
+    #region Enums
+
+    public enum SessionStatus
     {
-
-        #region Enums
-
-        public enum SessionStatus
-        {
-            Active,
-            Stashed,
-            Deleted
-        }
-
-        #endregion Enums
-
-        public DateTime SessionCreatedAt { get; set; }
-        public string SessionDescription { get; set; } = string.Empty;
-        public int SessionId { get; set; }
-        public string SessionName { get; set; } = string.Empty;
-        public double SessionTotalTimeMinutes { get; set; }
-        public SessionStatus Status { get; set; }
-        public List<string> Tags { get; set; } = new();
-        public List<SessionSteps> Tasks { get; set; } = new();
-        public double TotalTimesPlayed { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        Active,
+        Stashed,
+        Deleted
     }
+
+    #endregion Enums
+
+    public DateTime SessionCreatedAt { get; set; }
+    public string SessionDescription { get; set; } = string.Empty;
+    public int SessionId { get; set; }
+    public string SessionName { get; set; } = string.Empty;
+    public double SessionTotalTimeMinutes { get; set; }
+    public SessionStatus Status { get; set; }
+    public List<string> Tags { get; set; } = [];
+    public List<SessionSteps> Tasks { get; set; } = [];
+    public double TotalTimesPlayed { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
